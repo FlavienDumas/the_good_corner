8eb4f4d06d78fa131941a6dd5094c32c6a9b7208
FlavinouTheOne <flavien.dumas63@gmail.com>
2023-11-29 10:06:16 +0100

    added dockerisation + postegresql + cleaned database

--- backend/src/datasource.ts
+++ backend/src/datasource.ts
@@ -4,8 +4,12 @@ import { Category } from "./entities/Category";
 import { Tag } from "./entities/Tag";
 
 export const dataSource = new DataSource({
-  type: "sqlite",
-  database: "tgc.sqlite",
+  type: "postgres",
+  host: "db",
+  port: 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [Ad, Category, Tag],
   synchronize: true,
   logging: false
